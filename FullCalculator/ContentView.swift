f6ecefba4de41cc572e160881fdedeef1f13b267
Coding Bee Academy <rian.lesmana@codingbee.org>
2026-08-31 14:12:54 +0700

    Initial Commit

---
+++ FullCalculator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FullCalculator
+//
+//  Created by mac on 31/08/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FullCalculator/FullCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  FullCalculatorApp.swift
+//  FullCalculator
+//
+//  Created by mac on 31/08/26.
+//
+
+import SwiftUI
+
+@main
+struct FullCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
